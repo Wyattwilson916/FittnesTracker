0875ad983a409647df7b87cca05793b53f84a08d
Joel <joel.t.block@gmail.com>
2022-04-05 13:17:23 -0500

    fixed drop and create tables

--- db/client.js
+++ db/client.js
@@ -1,2 +1,7 @@
 // build and export your unconnected client here
+const { Client } = require("pg");
+const connection = "postgres://localhost:5432/fitness-dev";
 
+const client = new Client(connection);
+
+module.exports = client;
