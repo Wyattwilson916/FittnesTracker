fd360908859b45113741e47f45ef41383f5a5283
Joel <joel.t.block@gmail.com>
2022-04-06 11:25:08 -0500

    added routines db functions

--- db/activities.js
+++ db/activities.js
@@ -1,56 +1,82 @@
-const client = require("./client")
+const client = require("./client");
 
-async function createActivity({ name, description }){
-    try {
-        const { rows: [activity] } = await client.query(`
+async function createActivity({ name, description }) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
             INSERT INTO activities (name, description)
             VALUES ($1, $2)
             RETURNING *;
-        `, [name, description]);
-        return activity
-    } catch (error) {
-        console.error('Problem creating activity...', error)
-    }
+        `,
+      [name, description]
+    );
+    return activity;
+  } catch (error) {
+    console.error("Problem creating activity...", error);
+  }
 }
 
-async function getAllActivities(){
-    try {
-        const { rows: activities } = await client.query(`
+async function getAllActivities() {
+  try {
+    const { rows: activities } = await client.query(`
             SELECT *
             FROM activities;
         `);
-        return activities
-    } catch (error) {
-        console.error('Problem returning activities', error)
-    }
+    return activities;
+  } catch (error) {
+    console.error("Problem returning activities", error);
+  }
 }
 
-async function updateActivity({id, name, description}){
-    const fields = {name, description}
-    const setString = Object.keys(fields)
+async function updateActivity({ id, name, description }) {
+  const fields = { name, description };
+  const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
     .join(", ");
 
   if (setString.length === 0) {
     return;
   }
-    try {
-        const { rows: [activity] } = await client.query(`
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
             UPDATE activities
             SET ${setString}
             WHERE id=${id}
             RETURNING *;
-        `, Object.values(fields));
-        return activity
-    } catch (error) {
-        console.error('Problem updating activities', error)
-    }
+        `,
+      Object.values(fields)
+    );
+    return activity;
+  } catch (error) {
+    console.error("Problem updating activities", error);
+  }
 }
 
-
+async function getActivityById(id) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+        SELECT * FROM activities
+        WHERE id = $1;
+    `,
+      [id]
+    );
+    return activity;
+  } catch (error) {
+    console.error("Problem getting activity by id", error);
+  }
+}
 
 module.exports = {
-    createActivity,
-    getAllActivities,
-    updateActivity,
-}
+  createActivity,
+  getAllActivities,
+  updateActivity,
+  getActivityById,
+};
--- db/routines.js
+++ db/routines.js
@@ -1,36 +1,146 @@
-const client = require("./client")
+const client = require("./client");
 
-async function createRoutine({ creatorId, isPublic, name, goal }){
-    try {
-        const { rows: [routine] } = await client.query(`
+async function createRoutine({ creatorId, isPublic, name, goal }) {
+  try {
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
             INSERT INTO routines ("creatorId", "isPublic", name, goal)
             VALUES ($1, $2, $3, $4)
             RETURNING *;
-        `, [creatorId, isPublic, name, goal]);
-        return routine
-    } catch (error) {
-        console.error('Problem creating routines...', error)
-    }
+        `,
+      [creatorId, isPublic, name, goal]
+    );
+    return routine;
+  } catch (error) {
+    console.error("Problem creating routines...", error);
+  }
 }
 // async function getRoutineById(id){
 
 // }
-async function getRoutinesWithoutActivities(){
-    try {
-        const { rows: routines } = await client.query(`
+async function getRoutinesWithoutActivities() {
+  try {
+    const { rows: routines } = await client.query(`
             SELECT *
             FROM routines;
         `);
-        return routines
-    } catch (error) {
-        console.error('Problem returning routines w/o activities', error)
-    }
+    return routines;
+  } catch (error) {
+    console.error("Problem returning routines w/o activities", error);
+  }
 }
 
+function mapOverRoutines(routines) {
+  const map = {};
 
+  for (let routine of routines) {
+    if (!map[routine.id]) {
+      map[routine.id] = {
+        id: routine.id,
+        creatorId: routine.creatorId,
+        creatorName: routine.creatorName,
+        isPublic: routine.isPublic,
+        name: routine.name,
+        goal: routine.goal,
+        activities: [],
+      };
+      if (routine.activityId) {
+        map[routine.id].activities.push({
+          activityId: routine.activityId,
+          activityName: routine.activityName,
+          description: routine.description,
+          duration: routine.duration,
+          count: routine.count,
+        });
+      }
+    } else {
+      map[routine.id].activities.push({
+        activityId: routine.activityId,
+        activityName: routine.activityName,
+        description: routine.description,
+        duration: routine.duration,
+        count: routine.count,
+      });
+    }
+  }
+  return Object.values(map);
+}
 
+async function getAllRoutines() {
+  try {
+    const { rows } = await client.query(`
+        SELECT routines.id,
+            routines."creatorId",
+            users.username AS "creatorName",
+            routines."isPublic",
+            routines.name,
+            routines.goal,
+            activities.id AS "activityId",
+            activities.name AS "activityName",
+            activities.description,
+            routine_activities.duration,
+            routine_activities.count
+        FROM users
+        JOIN routines
+        ON users.id = routines."creatorId"
+        LEFT JOIN routine_activities
+        ON routines.id = routine_activities."routineId"
+        LEFT JOIN activities
+        ON routine_activities."activityId" = activities.id;
+        `);
+
+    return mapOverRoutines(rows);
+  } catch (error) {
+    console.error("Problem getting all routines", error);
+  }
+}
+
+async function getAllPublicRoutines() {
+  const routines = await getAllRoutines();
+  const publicRoutines = routines.filter((routine) => {
+    return routine.isPublic;
+  });
+
+  return publicRoutines;
+}
+
+async function getAllRoutinesByUser({ username }) {
+  const routines = await getAllRoutines();
+  const userRoutines = routines.filter((routine) => {
+    return routine.creatorName === username;
+  });
+
+  return userRoutines;
+}
+
+async function getPublicRoutinesByUser({ username }) {
+  const userRoutines = await getAllRoutinesByUser({ username });
+  const publicRoutines = userRoutines.filter((routine) => {
+    return routine.isPublic;
+  });
+
+  return publicRoutines;
+}
+
+async function getPublicRoutinesByActivity({ id }) {
+  const routines = await getAllPublicRoutines();
+  const activityRoutines = routines.filter((routine) => {
+    for (let activity of routine.activities) {
+      return activity.activityId === id;
+    }
+  });
+
+  return activityRoutines;
+}
 
 module.exports = {
-    createRoutine,
-    getRoutinesWithoutActivities,
-}
+  createRoutine,
+  getRoutinesWithoutActivities,
+  getAllRoutines,
+  getAllPublicRoutines,
+  getAllRoutinesByUser,
+  getPublicRoutinesByUser,
+  getPublicRoutinesByActivity,
+};
