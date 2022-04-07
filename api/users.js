c2076a0ae99021e2242f33c02c287b35f3bddcdc
Joel <joel.t.block@gmail.com>
2022-04-07 08:49:16 -0500

    users/register

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const usersRouter = express.Router();
-const { createUser } = require("../db");
+const { createUser, getUserByUsername } = require("../db");
 
 usersRouter.post("/register", async (req, res, next) => {
   const { username, password } = req.body;
@@ -8,6 +8,12 @@ usersRouter.post("/register", async (req, res, next) => {
     throw new Error("Password too short");
   }
   try {
+    // check if username taken
+    let _user = await getUserByUsername(username);
+    if (_user.id) {
+      throw new Error("Username already exists");
+    }
+    // create new user
     const user = await createUser({ username, password });
     res.send(user);
   } catch (error) {
