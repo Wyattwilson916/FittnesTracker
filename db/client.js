4cca54e45f684f76a42052b2388901e80e3ed815
Joel <joel.t.block@gmail.com>
2022-04-15 14:37:43 -0500

    final changes for heroku

--- db/client.js
+++ db/client.js
@@ -2,6 +2,6 @@
 const { Client } = require("pg");
 const connection = "postgres://localhost:5432/fitness-dev";
 
-const client = new Client(connection);
+const client = new Client(process.env.DATABASE_URL || connection);
 
 module.exports = client;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const server = express();
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const morgan = require("morgan");
 
 server.use(morgan("dev"));
@@ -12,21 +12,12 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use((req, res, next) => {
-  console.log("<____Body Logger START____>");
-  console.log(req.body);
-  console.log("<_____Body Logger END_____>");
-
-  next();
-});
-
 const apiRouter = require("./api");
 server.use("/api", apiRouter);
 
 const client = require("./db/client");
 
 server.use((error, req, res, next) => {
-  // console.error("SERVER ERROR: ", error);
   if (res.statusCode < 400) res.status(500);
   res.send({
     error: error.message,
@@ -38,5 +29,5 @@ server.use((error, req, res, next) => {
 
 client.connect();
 server.listen(PORT, () => {
-  console.log(`Server is listening at localhost:${PORT}`);
+  console.log(`Server is listening at ${PORT}`);
 });
